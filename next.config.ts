56e06de8ca04865437c2720d80b6cfcef45ea927
psgoularte <sgoularte.pedro@gmail.com>
2025-07-27 18:06:36 -0300

    arquivos base do front e back

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+/** @type {import{'next'}.NextConfig} */
+const nextConfig = {
+  webpack: (config: { externals: string[] }) => {
+    config.externals.push("pino.pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
